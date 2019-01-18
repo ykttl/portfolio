c47ef99dbed54d836bbd7ba6ee67fca926ecc1a7
Yuko <yuko.mazurek@gmail.com>
2019-01-18 01:47:15 -0800

    Updates

---
+++ precache-manifest.9e69f75d68cf45bab3b1689826e17540.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "d36dbc4432a53e01fd89",
+    "url": "/website/static/js/main.d36dbc44.chunk.js"
+  },
+  {
+    "revision": "a726c333e0b765a4228a",
+    "url": "/website/static/js/1.a726c333.chunk.js"
+  },
+  {
+    "revision": "5b585062a8787af9f7cb8f89bffc1883",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.70bc0722186080ae69b0cbc59499df29.js"
+  "/website/precache-manifest.9e69f75d68cf45bab3b1689826e17540.js"
 );
 
 workbox.clientsClaim();

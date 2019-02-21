c69c608c6ef446d7b57c2bbbdc42dbc01810fc99
Yuko <yuko.mazurek@gmail.com>
2019-02-20 20:55:58 -0800

    Updates

--- precache-manifest.46ed86d5532918b7b76dd8013000f767.js
+++ precache-manifest.46ed86d5532918b7b76dd8013000f767.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "ca7fb72c3f018e4ba241",
-    "url": "/portfolio/static/js/main.ca7fb72c.chunk.js"
+    "revision": "220059df19cb95c1552a",
+    "url": "/portfolio/static/js/main.220059df.chunk.js"
   },
   {
     "revision": "369dc987d0e0046aa52b",
     "url": "/portfolio/static/js/1.369dc987.chunk.js"
   },
   {
-    "revision": "ca7fb72c3f018e4ba241",
+    "revision": "220059df19cb95c1552a",
     "url": "/portfolio/static/css/main.22c88544.chunk.css"
   },
   {
-    "revision": "fa19b22d854ebdf81f61e457d3d4b6f0",
+    "revision": "6735f7c531216d2a883216fd81227655",
     "url": "/portfolio/index.html"
   }
 ];

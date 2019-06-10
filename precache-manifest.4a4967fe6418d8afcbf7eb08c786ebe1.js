1ddb46b7c63337b8ff646e8b6885f331d378e4bf
Yuko <yuko.mazurek@gmail.com>
2019-06-10 01:10:02 -0700

    Updates

---
+++ precache-manifest.4a4967fe6418d8afcbf7eb08c786ebe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41382575d4665264c627ee69f75a92a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7842a61d56ce45763a09",
+    "url": "/portfolio/static/css/main.b61e6d4b.chunk.css"
+  },
+  {
+    "revision": "a5472fba01277455f5e2",
+    "url": "/portfolio/static/js/2.4ac062d8.chunk.js"
+  },
+  {
+    "revision": "7842a61d56ce45763a09",
+    "url": "/portfolio/static/js/main.58793e4a.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);

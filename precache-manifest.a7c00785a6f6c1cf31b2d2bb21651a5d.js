ddbdcf51ad5c94812d776c7b77907e71554b01bd
Yuko <yuko.mazurek@gmail.com>
2021-01-28 00:27:28 -0800

    Updates

--- precache-manifest.a7c00785a6f6c1cf31b2d2bb21651a5d.js
+++ precache-manifest.a7c00785a6f6c1cf31b2d2bb21651a5d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a41382575d4665264c627ee69f75a92a",
+    "revision": "032db1405e6185d7230cfd8235ff1828",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "7842a61d56ce45763a09",
-    "url": "/portfolio/static/css/main.b61e6d4b.chunk.css"
+    "revision": "db282ed55c0895ff4b7d",
+    "url": "/portfolio/static/css/main.8091d23a.chunk.css"
   },
   {
     "revision": "a5472fba01277455f5e2",
     "url": "/portfolio/static/js/2.4ac062d8.chunk.js"
   },
   {
-    "revision": "7842a61d56ce45763a09",
-    "url": "/portfolio/static/js/main.58793e4a.chunk.js"
+    "revision": "db282ed55c0895ff4b7d",
+    "url": "/portfolio/static/js/main.e5f195de.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4a4967fe6418d8afcbf7eb08c786ebe1.js"
+  "/portfolio/precache-manifest.a7c00785a6f6c1cf31b2d2bb21651a5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
